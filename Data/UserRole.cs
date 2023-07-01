b373f8c65e5c44eeae822d7b491281662b7371e2
Magdalena Trzcińska <s22536@pjwstk.edu.pl>
2023-07-01 17:44:18 +0200

    Add roles

---
+++ Data/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace VeterinarySystem.Data
+{
+    public static class UserRole
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee= "Employee";
+    }
+}

a0c2231a82e66a186a6c41d3f43df5e4f7130764
Magdalena Trzcińska <s22536@pjwstk.edu.pl>
2023-07-21 16:57:14 +0200

    js file separation

---
+++ wwwroot/js/calendars.js
@@ -0,0 +1,27 @@
+﻿function loadCalendarOfAppointments() {
+    var calendarEl = document.getElementById("calendar");
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: "listMonth",
+        navLinks: true,
+        dayMaxEvents: true,
+        headerToolbar: {
+            center: "addEventButton"
+        },
+        customButtons: {
+            addEventButton: {
+                text: "Add appointment",
+                click: function () {
+                    $("#addEventModal").modal("show");
+                }
+            }
+        },
+        events: "/Employee/Animal/GetAllAppointments/" + animalId,
+        eventClick: function (info) {
+            //info.event.id;
+            $("#exampleModal").modal("show");
+        },
+    });
+    calendar.render();
+};
+
+loadCalendarOfAppointments();
---
+++ wwwroot/js/charts.js
@@ -0,0 +1,47 @@
+﻿const ctx = document.getElementById("chart");
+
+const config = {
+    type: "line",
+    data: {
+        labels: JSON.parse(datesOfWeightsChart),
+        datasets: [{
+            label: "",
+            data: JSON.parse(valuesOfWeightChart),
+            pointStyle: "circle",
+            borderColor: "#593196",
+            pointRadius: 5,
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        onClick: function (evt) {
+            const points = chartLine.getElementsAtEventForMode(evt, "nearest", { intersect: true }, true);
+            if (points.length) {
+                const firstPoint = points[0];
+                const label = chartLine.data.labels[firstPoint.index];
+
+                var answer = prompt("Enter a new value. To delete - leave blank");
+                var value = parseInt(answer) ?? 0;
+
+                $.ajax({
+                    type: "POST",
+                    url: "/Employee/Animal/UpdateWeights",
+                    data: {
+                        label, value, id: animalId
+                    },
+                    success: function (result) {
+                        window.location.href = "/Employee/Animal/Detail/" + animalId;
+                    },
+                    error: function (error) {
+                        console.error(error);
+                    }
+                });
+            }
+        }
+    }
+}
+const chartLine = new Chart(ctx, config);

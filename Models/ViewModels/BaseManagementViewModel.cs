703e4b0fecab262e111637a83acd8840e6d1d0b4
Magdalena Trzcińska <s22536@pjwstk.edu.pl>
2023-07-31 13:45:14 +0200

    rename

--- Models/ViewModels/BaseManagementViewModel.cs
+++ Models/ViewModels/BaseManagementViewModel.cs
@@ -4,9 +4,9 @@ using VeterinarySystem.Models.Db;
 namespace VeterinarySystem.Models.ViewModels
 {
     public class BaseManagementViewModel
-    { 
+    {
         [ValidateNever]
-        public IEnumerable<Breed> Breeds { get; set; } = new List<Breed>();
-        public Breed NewBreed { get; set; } = null!;
+        public IEnumerable<Breed> AllBreeds { get; set; } = new List<Breed>();
+        public Breed NewBreed { get; set; } = new Breed();
     }
 }

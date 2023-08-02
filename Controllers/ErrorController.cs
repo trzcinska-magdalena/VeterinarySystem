813e7224db9c182d5cc7177d6faa68857f8509b9
Magdalena Trzcińska <s22536@pjwstk.edu.pl>
2023-08-02 16:47:25 +0200

    add error controller

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VeterinarySystem.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error")]
+        public IActionResult Index()
+        {
+
+            return View();
+
+        }
+    }
+}
